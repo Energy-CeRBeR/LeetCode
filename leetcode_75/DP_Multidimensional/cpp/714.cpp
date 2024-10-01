9f1a920db9a167edd2ff1c78fce686b741da468d
Igor Chesnykh <igor.chesnyx@mail.ru>
2024-10-01 19:02:25 +0300

    Solved all on DP_Multidimensional

---
+++ leetcode_75/DP_Multidimensional/cpp/714.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices, int fee)
+    {
+        int buy = INT_MIN;
+        int sell = 0;
+
+        for (int price : prices)
+        {
+            buy = max(buy, sell - price);
+            sell = max(sell, buy + price - fee);
+        }
+
+        return sell;
+    }
+};
