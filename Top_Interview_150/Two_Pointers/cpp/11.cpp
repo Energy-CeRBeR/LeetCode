94fe4afb49de83035f360b7725ad8a2e324ef0a4
Igor Chesnykh <igor.chesnyx@mail.ru>
2024-10-14 12:05:35 +0300

    Solved all problems on two pointers; in my leetcode 100 problems has been solved :)

---
+++ Top_Interview_150/Two_Pointers/cpp/11.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int left = 0;
+        int right = height.size() - 1;
+
+        int mx = 0;
+        while (left < right)
+        {
+            mx = max(mx, min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return mx;
+    }
+};
